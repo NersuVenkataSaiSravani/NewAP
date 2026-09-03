779f0eb1cd5b4edfde279079694fc9c4d1a70da3
NersuVenkataSaiSravani <147939468+NersuVenkataSaiSravani@users.noreply.github.com>
2026-09-03 17:53:41 +0530

    Update apiap.py

--- apiap.py
+++ apiap.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/') #http://localhost:5000
 def home():
   return "API is running"
 
 @app.route('/hello')
 def hello():
-  return "Hello, this is hello page"
+  return "Hello, this is hello page"   #http://localhost:5000/hello
 
 if __name__=="__main__":
   app.run(host="0.0.0.0",port=5000)
